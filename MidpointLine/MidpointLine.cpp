2913507fe7f8ca84343756d132744e20305cbb76
Joliosaurio <julio_sanchez@outlook.com>
2017-08-23 03:43:04 -0500

    Add files via upload

---
+++ MidpointLine/MidpointLine.cpp
@@ -0,0 +1,17 @@
+#include "Application.h"
+#include <iostream>
+
+#define PI  3.1516
+void Application::update()
+{
+	
+}
+
+void Application::draw()
+{
+	setColor(255, 255, 0, 255);
+	//linea(256, 256, 100, 100);
+	MidPoint(256, 256, 200);
+	clearScreen();
+}
+
